e4a6db90ea6173bcda76d2f747eb5d4dff0a196a
shrihari <shriharikodiyala4@gmail.com>
2025-11-18 23:13:34 +0530

    Updated client UI and CSS

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    transformer: "postcss" // Disable lightningcss processor
-  },
-  build: {
-    cssMinify: false // Disable lightningcss minifier
-  }
 });
